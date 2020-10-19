0d764021f508ab83564fea0068eeb8ec65e112bc
Wai Min <leopard23@gmail.com>
2020-10-19 21:53:00 +0800

    Implement route for querying bookings of a month

--- server/controller/bookings.js
+++ server/controller/bookings.js
@@ -2,13 +2,26 @@ const bookingsRouter = require('express').Router();
 const Booking = require('../models/booking');
 
 bookingsRouter.get('/', async (req, res) => {
-  const bookings = await Booking.find({});
-  return res.send(bookings);
+  const query = req.query;
+  if (Object.keys(query).length !== 0) {
+    const month = query.month;
+    const year = query.year;
+    const startDate = new Date(Number(year), Number(month), 1);
+    const endDate = new Date(Number(year), Number(month) + 1, 0);
+
+    const bookings = await Booking.find({
+      date: { $gte: startDate, $lte: endDate },
+    });
+    return res.json(bookings);
+  } else {
+    const bookings = await Booking.find({});
+    return res.json(bookings);
+  }
 });
 
 bookingsRouter.get('/:id', async (req, res) => {
   const booking = await Booking.findById(req.params.id);
-  if (booking) return res.send(bookings);
+  if (booking) return res.json(booking);
   else return res.status(404).end();
 });
 
@@ -22,6 +35,7 @@ bookingsRouter.post('/', async (req, res) => {
     start: body.start,
     end: body.end,
   });
+  console.log('date', body.date);
 
   const savedBooking = await booking.save();
   res.json(savedBooking);
