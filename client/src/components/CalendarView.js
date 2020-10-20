50effb543dba322179b13011ffb6ea0308260b35
Wai Min <leopard23@gmail.com>
2020-10-20 23:07:23 +0800

    Fetch current month bookings at CalendarView

--- client/src/components/CalendarView.js
+++ client/src/components/CalendarView.js
@@ -1,10 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import AllSlots from './AllSlots';
+import bookingService from '../services/bookings';
 
 export default function CalendarView() {
   const [date, onChange] = useState(new Date());
+
+  useEffect(() => {
+    const fetchCurMonth = async () => {
+      await bookingService.getCurMonthBookings();
+    };
+    fetchCurMonth();
+  });
 
   return (
     <div>
