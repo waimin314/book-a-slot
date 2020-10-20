5f516137cccd111709c45fdcc08651e4d4b210ba
Wai Min <leopard23@gmail.com>
2020-10-20 23:08:33 +0800

    Save filtered bookings of current month after fetching

--- client/src/services/bookings.js
+++ client/src/services/bookings.js
@@ -2,10 +2,11 @@ import axios from 'axios';
 
 const baseUrl = '/api/v1/bookings';
 
+const bookingsByMonth = {};
+
 const getBookingsByMonth = async (month, year) => {
   const response = await axios.get(`${baseUrl}?month=${month}&year=${year}`);
 
-  console.log(`Bookings of month ${month} `, response);
   return response;
 };
 
@@ -15,9 +16,21 @@ const getCurMonthBookings = async () => {
     date.getMonth(),
     date.getFullYear()
   );
+  console.log('getCurMonthBookings -> response', response);
 
-  console.log(`Bookings of current month ${date.getMonth()} `, response);
+  bookingsByMonth[date.getMonth()] = sanitize(response.data);
+  console.log(bookingsByMonth);
+
   return response;
+};
+
+const sanitize = (bookings) => {
+  return bookings.map((booking) => {
+    return {
+      date: booking.date,
+      duration: `${booking.start}-${booking.end}`,
+    };
+  });
 };
 
 export default { getBookingsByMonth, getCurMonthBookings };
