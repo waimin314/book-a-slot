17904203e59343f0cc54b89a649488e5ed0cb17d
Wai Min <leopard23@gmail.com>
2020-10-20 20:30:46 +0800

    Add tests for POST route

--- server/tests/booking_api.test.js
+++ server/tests/booking_api.test.js
@@ -53,6 +53,44 @@ describe('When retrieving a booking with ID', () => {
   });
 });
 
+describe('When adding a booking', () => {
+  test('a booking with valid details should be successful', async () => {
+    const newBooking = {
+      name: 'Bruce Wayne',
+      email: 'bruce@wayneent.com',
+      date: new Date(),
+      start: '19:30',
+      end: '21:30',
+    };
+    await api
+      .post('/api/v1/bookings')
+      .send(newBooking)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    const bookingsAfterPost = await helper.bookingsInDb();
+    expect(bookingsAfterPost).toHaveLength(helper.initialBookings.length + 1);
+
+    const lastBookingInDb = bookingsAfterPost[bookingsAfterPost.length - 1];
+    expect(lastBookingInDb.date).toEqual(newBooking.date);
+  });
+
+  test('a booking with missing details should fail with status 400', async () => {
+    const newBooking = {
+      email: 'Ihavenoname@noname.com',
+      date: new Date(),
+      start: '18:30',
+      end: '19:00',
+    };
+
+    await api.post('/api/v1/bookings').send(newBooking).expect(400);
+
+    const bookingsAfterPost = await helper.bookingsInDb();
+
+    expect(bookingsAfterPost).toHaveLength(helper.initialBookings.length);
+  });
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
