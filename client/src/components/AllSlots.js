8fcc5ef09c09d2759cb77d14ef1a861cec2cb810
Wai Min <leopard23@gmail.com>
2020-10-22 22:33:37 +0800

    Refactor rendering of the slots

--- client/src/components/AllSlots.js
+++ client/src/components/AllSlots.js
@@ -2,11 +2,9 @@ import React from 'react';
 import Slot from './Slot';
 
 export default function AllSlots({ timings }) {
-  return (
-    <div>
-      {timings.map((timing) => (
-        <Slot timing={timing} />
-      ))}
-    </div>
-  );
+  const renderTimings = () => {
+    return timings.map((timing) => <Slot timing={timing} />);
+  };
+
+  return <div>{renderTimings()}</div>;
 }
