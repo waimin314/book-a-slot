0300b367c4da85d1472e566f330e586aff184dab
Wai Min <leopard23@gmail.com>
2020-10-22 23:56:34 +0800

    Remove redundant AllSlots component

--- client/src/components/CalendarView.js
+++ client/src/components/CalendarView.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import AllSlots from './AllSlots';
+import Slot from './Slot';
 import bookingService from '../services/bookings';
 
 export default function CalendarView() {
   const [date, setDate] = useState(new Date());
   const [bookingsOfDate, setBookingsOfDate] = useState([]);
+  const [selectedSlot, setSelectedSlot] = useState('');
 
   useEffect(() => {
     const fetchCurMonth = async () => {
@@ -18,14 +19,30 @@ export default function CalendarView() {
 
   const handleDateChange = (date) => {
     setDate(date);
+    setSelectedSlot('');
     // console.log(bookingService.getBookingsByDate(date));
     setBookingsOfDate([...bookingService.getBookingsByDate(date)]);
+  };
+
+  const handleOnSelect = (slot) => {
+    setSelectedSlot(slot);
+  };
+
+  const renderAllSlots = () => {
+    return bookingsOfDate.map((booking) => {
+      if (booking.booked) {
+        return <Slot timing={booking} />;
+      } else {
+        return <Slot timing={booking} onSelect={handleOnSelect} />;
+      }
+    });
   };
 
   return (
     <div>
       <Calendar onChange={handleDateChange} value={date} />
-      <AllSlots timings={bookingsOfDate} />
+      <div>Selected {selectedSlot}</div>
+      {renderAllSlots()}
     </div>
   );
 }
