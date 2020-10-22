d8ca0bd902791bc9d5109d1f300475b6b9303a69
Wai Min <leopard23@gmail.com>
2020-10-22 22:29:08 +0800

    Show booked slots in different color

--- client/src/components/AllSlots.js
+++ client/src/components/AllSlots.js
@@ -4,7 +4,6 @@ import Slot from './Slot';
 export default function AllSlots({ timings }) {
   return (
     <div>
-      {console.log('AllSlots -> timings', timings)}
       {timings.map((timing) => (
         <Slot timing={timing} />
       ))}
--- client/src/components/Slot.js
+++ client/src/components/Slot.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 export default function Slot({ timing }) {
   return (
-    <div style={{ width: 'auto', border: '1px solid black' }}>{timing}</div>
+    <div
+      style={{
+        display: 'inline-block',
+        width: 'auto',
+        border: '1px solid black',
+        backgroundColor: timing.booked ? 'grey' : 'white',
+        color: timing.booked ? 'white' : 'black',
+      }}
+    >
+      {timing.time}
+    </div>
   );
 }
