29319dd60820cb0fc37a6d5767d2eabd21b2459d
Wai Min <leopard23@gmail.com>
2020-10-23 21:38:59 +0800

    Configure tailwindcss

--- client/src/components/Slot.js
+++ client/src/components/Slot.js
@@ -2,16 +2,7 @@ import React from 'react';
 
 export default function Slot({ timing, onSelect }) {
   return (
-    <div
-      style={{
-        display: 'inline-block',
-        width: 'auto',
-        border: '1px solid black',
-        backgroundColor: timing.booked ? 'grey' : 'white',
-        color: timing.booked ? 'white' : 'black',
-      }}
-      onClick={() => (onSelect ? onSelect(timing.time) : {})}
-    >
+    <div onClick={() => (onSelect ? onSelect(timing.time) : {})}>
       {timing.time}
     </div>
   );
