b3a6f506137d2d6d0e03ae350c182413ea7d759c
Wai Min <leopard23@gmail.com>
2020-10-26 20:50:13 +0800

    Gray out booked slots

--- client/src/components/Slot.js
+++ client/src/components/Slot.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Slot({ timing, onSelect }) {
+  const handleOnClick = () => {
+    onSelect && onSelect(timing.time);
+  };
+
   return (
     <div
-      className='w-32 p-2 m-2 bg-blue-300 text-center rounded-lg text-lg'
-      onClick={() => (onSelect ? onSelect(timing.time) : {})}
+      className={`w-32 p-2 m-2 ${
+        timing.booked ? 'bg-gray-400 text-gray-600' : 'bg-blue-300'
+      } text-center rounded-lg text-lg`}
+      onClick={handleOnClick}
     >
       {timing.time}
     </div>
