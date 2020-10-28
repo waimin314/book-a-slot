0bce34e19566856ffee1bdb6be6d777ef844f606
Wai Min <leopard23@gmail.com>
2020-10-28 21:01:23 +0800

    Add build folder

---
+++ server/build/precache-manifest.939588d7d3be3910ac85592ce4c42c55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61bc9609788a04f66c59d3647c2d1292",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e853970192f180ab2ca",
+    "url": "/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "b62d67822b16788ff2a8",
+    "url": "/static/css/main.17d91aac.chunk.css"
+  },
+  {
+    "revision": "6e853970192f180ab2ca",
+    "url": "/static/js/2.040a6005.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.040a6005.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b62d67822b16788ff2a8",
+    "url": "/static/js/main.9bc1b13f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
